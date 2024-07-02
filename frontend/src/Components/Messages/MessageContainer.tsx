2930888df43260904c1c1f158e4928a684263d49
Vansh Arora <147429499+vansh-9878@users.noreply.github.com>
2024-07-02 17:27:36 +0530

    Create MessageContainer.tsx

---
+++ frontend/src/Components/Messages/MessageContainer.tsx
@@ -0,0 +1,48 @@
+import { useContext, useEffect } from "react";
+import NoChat from "../NoChat";
+import MessageInput from "./MessageInput";
+import Messages from "./Messages";
+import { ConvoContext } from "../../context/ConvoContext";
+import { GoDotFill } from "react-icons/go";
+import { SocketContext } from "../../context/SocketContext";
+
+const MessageContainer = () => {
+  const { selectedConvo, setSelectedConvo } = useContext(ConvoContext);
+  const { onlineUsers } = useContext(SocketContext);
+  let isOnline = false;
+  if (selectedConvo) {
+    isOnline = onlineUsers.includes(selectedConvo._id);
+  }
+  console.log(selectedConvo);
+
+  useEffect(() => {
+    return () => setSelectedConvo(null);
+  }, [setSelectedConvo]);
+  return (
+    <>
+      {!selectedConvo ? (
+        <NoChat />
+      ) : (
+        <div className="message-container w-100 ">
+          <div className="bg-light">
+            <div className="fs-4 ms-2">
+              <span className="">{selectedConvo.fullname}</span>
+            </div>
+            <div
+              className="ms-2 "
+              style={{ color: `${isOnline ? "green" : "grey"}` }}
+            >
+              <GoDotFill />
+              {isOnline ? "Online" : "Offline"}
+            </div>
+          </div>
+
+          <Messages />
+          <MessageInput />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MessageContainer;
