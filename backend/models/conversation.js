c8383f36a8e88ce26141b963846d030c924e5e72
Vansh Arora <147429499+vansh-9878@users.noreply.github.com>
2024-07-02 17:21:37 +0530

    Create conversation.js

---
+++ backend/models/conversation.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const conversationSchema = mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    messages: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+        default: [],
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Conversation", conversationSchema);
