2ed23a173fd9cceb5094c45a53185403f7ff724b
Vansh Arora <147429499+vansh-9878@users.noreply.github.com>
2024-07-02 17:26:11 +0530

    Add files via upload

---
+++ frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext.tsx";
+import { SocketContextProvider } from "./context/SocketContext.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <SocketContextProvider>
+        <App />
+      </SocketContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
