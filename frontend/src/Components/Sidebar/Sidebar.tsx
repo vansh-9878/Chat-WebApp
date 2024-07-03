b124312236d254f0f032fecf7f3485241915cd4b
Vansh Arora <147429499+vansh-9878@users.noreply.github.com>
2024-07-03 10:27:48 +0530

    Update Sidebar.tsx

--- frontend/src/Components/Sidebar/Sidebar.tsx
+++ frontend/src/Components/Sidebar/Sidebar.tsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
     <div className="ms-2">
       <SearchInput />
       <div
-        className="w-75 bg-secondary-subtle "
+        className="w-100 bg-secondary-subtle "
         style={{ height: "1.4px" }}
       ></div>
       <ConversationList />
