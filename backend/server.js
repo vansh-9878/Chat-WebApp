49a9caad62e46e2763722c4292c04d7082bdf79b
Vansh Arora <147429499+vansh-9878@users.noreply.github.com>
2024-08-03 13:39:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const { app, server } = require("./socket/socket");
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
